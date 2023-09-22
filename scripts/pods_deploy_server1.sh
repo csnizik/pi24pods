e3d11e055aebd7e958b232d912ce5a54f9016d0e
Forsyth Bacon <Forsyth.Bacon@usda.gov>
2023-09-22 10:04:32 -0500

    Pull request #83: Story/CIGCPDPH2 1279 pods deploy script add a reinstall or forceinstall option
    Merge in NRCSAPPS/pods from story/CIGCPDPH2-1279-pods-deploy-script-add-a-reinstall-or-forceinstall-option to REL_1.2
    
    Squashed commit of the following:
    
    commit 599f47f29d8cb6fd2d5cecad9e5001f3211d7281
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Thu Sep 21 15:26:41 2023 -0700
    
        Removed verbose option from the tar extract
    
    commit 1b93e854ea6a99c076e973b5d6d7c18fd873e9bf
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Thu Sep 21 15:12:44 2023 -0600
    
        adjusted the pods server 1 deploy script to deploy correctly
    
    commit 01334122b42fc7947c84dbbb37c4b8db41ee824a
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Tue Sep 19 15:09:17 2023 -0700
    
        Corrected typo and this time the gitignore change
         included
    
    commit 066ffd0d0205a6ee712dd634258b00d57f1a2799
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Tue Sep 19 10:27:07 2023 -0700
    
        renaming of deploy scripts, change to more broadly ignore tar files
    
    commit 0b21c9b36e9b6f02d9156abc63551709dd64ba82
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Tue Sep 19 09:58:43 2023 -0700
    
        Adding another tarball script as work in progress
    
    commit d48ac54c53b6fbbe430adbc68f828f04adccb91a
    Author: forsyth.bacon@usda.gov <forsyth.bacon@usda.gov>
    Date:   Mon Sep 18 14:53:03 2023 -0700
    
        added wipe_db option

--- scripts/pods_deploy_server1.sh
+++ scripts/pods_deploy_server1.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
 SUPPORTED_ENVS='dev test uat stage prod'
-if [ $# -ne 3 ]; then
-        echo "Usage - <your_pods_tarball_file_version_number> <environment (dev/test/uat/stage/prod)> <your_drupal_password>"
+if [ $# -lt 3 ]; then
+        echo "Usage - <your_pods_tarball_file_version_number> <environment (dev/test/uat/stage/prod)> <your_drupal_password> [wipe_db]"
         exit 1
+fi
+
+if [ $# -eq 4 ] ; then
+  if [ $4 -eq "wipe_db" ] ; then
+
+    echo "All data will be deleted from the database";
+    read -r -p "PODS will be rebuilt. All data will be lost. Are you sure? [Y/n] " input
+    case $input in
+      [yY][eE][sS]|[yY])
+            echo "Rebuilding PODS including the database..."
+            ;;
+      [nN][oO]|[nN])
+            exit 0
+            ;;
+      *)
+            echo "Invalid input..."
+            exit 1
+            ;;
+
+    esac
+    #if here $wipe_db option was confirmed
+    $WIPE_DB = "true"
+  else
+    echo "Invalid option: $4"
+    exit 1
+  fi
 fi
 
 declare -A envs=( ["dev"]="PODS-Dev.Dev" ["test"]="pods" ["uat"]="pods" ["stage"]="pods" ["prod"]="pods" )
@@ -15,26 +41,27 @@ echo "PODS folder for deploy is: ${envs[$2]}" 2>&1|tee $LOGFILE
 LOGFILE="/app/upload/pods_$1_init.log"
 UPGRADE_SCENARIO="false"
 
-# Ensure that the required files have been uploaded to the /app/upload directory
-if [[ ! -f /app/upload/pods_$1.tar.gz ]]; then
-		echo "The file pods_$1.tar.gz does not exist in the /app/upload directory on this server."  2>&1|tee $LOGFILE
-		echo "Please download pods_$1.tar.gz to the /app/upload directory and run this script again."  2>&1|tee $LOGFILE
-		exit 1
-elif [[ ! -f /app/upload/pods.$2.settings.php ]]; then
-		echo "The file pods.$2.settings.php does not exist in the /app/upload directory on this server."  2>&1|tee $LOGFILE
-		echo "Please download pods.$2.settings.php to the /app/upload directory and run this script again."  2>&1|tee $LOGFILE
-		exit 1
-fi
-
 # If there is already a Pods installation, make a backup of the existing settings file.
 if [[ -f /app/www/html/"$ENVNAME"/web/sites/default/settings.php ]]; then
-        UPGRADE_SCENARIO="true"
-		echo "Creating a backup of the existing settings.php file." 2>&1|tee $LOGFILE
-		cp -fp /app/www/html/"$ENVNAME"/web/sites/default/settings.php /app/upload/pods."$2".settings.php.bak
+    UPGRADE_SCENARIO="true"
+                echo "Creating a backup of the existing settings.php file." 2>&1|tee $LOGFILE
+                cp -fp /app/www/html/"$ENVNAME"/web/sites/default/settings.php /app/upload/pods."$2".settings.php.bak
 else
-        echo "Creating $ENVNAME folder in /app/www/html" 2>&1|tee -a $LOGFILE
-        mkdir /app/www/html/"$ENVNAME" 2>&1|tee -a $LOGFILE
-        wait
+    echo "Creating $ENVNAME folder in /app/www/html" 2>&1|tee -a $LOGFILE
+    mkdir /app/www/html/"$ENVNAME" 2>&1|tee -a $LOGFILE
+    wait
+fi
+
+# Ensure that the required files have been uploaded to the /app/upload directory
+if [[ ! -f /app/upload/pods_$1.tar.gz ]]; then
+                echo "The file pods_$1.tar.gz does not exist in the /app/upload directory on this server."  2>&1|tee $LOGFILE
+                echo "Please download pods_$1.tar.gz to the /app/upload directory and run this script again."  2>&1|tee $LOGFILE
+                exit 1
+# add check of upgrade option
+elif [[ ! -f /app/upload/pods.$2.settings.php ]] && [[ $UPGRADE_SCENARIO == "false" ]]; then
+                echo "The file pods.$2.settings.php does not exist in the /app/upload directory on this server."  2>&1|tee $LOGFILE
+                echo "Please download pods.$2.settings.php to the /app/upload directory and run this script again."  2>&1|tee $LOGFILE
+                exit 1
 fi
 
 # Since the required files exist in the /app/upload directory, we can proceed with installing PODS on the server.
@@ -49,18 +76,20 @@ if [[ "$SUPPORTED_ENVS" == *"$2"* ]]; then
 
         cd /app/www/html/"$ENVNAME"
 
-        echo "Restoring the PODS tarball file: /app/upload/pods_$1.tar.gz" 2>&1|tee -a $LOGFILE
+        echo "Extracting the PODS tarball file: /app/upload/pods_$1.tar.gz" 2>&1|tee -a $LOGFILE
         tar -xzf /app/upload/pods_$1.tar.gz 2>&1|tee -a $LOGFILE
-		wait
+                wait
 
-        if [[ "$UPGRADE_SCENARIO" == "false" ]]; then
-            
-            echo "Copying the /app/upload/pods.$2.settings.php file to the settings file..." 2>&1|tee -a $LOGFILE
-            cp -fp /app/upload/pods.$2.settings.php /app/www/html/"$ENVNAME"/web/sites/default/settings.php 2>&1|tee -a $LOGFILE
-
+        if [[ "$UPGRADE_SCENARIO" == "false" || $WIPE_DB == "true" ]]; then
+            if [ $WIPE_DB == "true" ]; then
+              echo "Restoring the backed up settings.php file" 2>&1|tee -a $LOGFILE
+            else
+              echo "Copying the /app/upload/pods.$2.settings.php file to the settings file..." 2>&1|tee -a $LOGFILE
+              cp -fp /app/upload/pods.$2.settings.php /app/www/html/"$ENVNAME"/web/sites/default/settings.php 2>&1|tee -a $LOGFILE
+            fi
             echo "Running drush site-install command..." 2>&1|tee -a $LOGFILE
             /app/www/html/"$ENVNAME"/vendor/bin/drush site-install -y --existing-config --account-pass=$3 --site-name="NRCS Producer Operations Data System" farm farm.modules="base" 2>&1 | tee -a $LOGFILE
-    		wait
+                wait
 
             echo "Running drush config-set for css.preprocess..." 2>&1|tee -a $LOGFILE
             /app/www/html/"$ENVNAME"/vendor/bin/drush  -y config-set  system.performance css.preprocess 0 2>&1|tee -a $LOGFILE
@@ -69,15 +98,15 @@ if [[ "$SUPPORTED_ENVS" == *"$2"* ]]; then
 
             echo "Running drush to clear and rebuild cache..." 2>&1|tee -a $LOGFILE
             /app/www/html/"$ENVNAME"/vendor/bin/drush cr 2>&1|tee -a $LOGFILE
-		    wait
+                    wait
 
             echo "Running /app/www/html/"$ENVNAME"/vendor/bin/drush en -y structure_sync farm_map_mapbox cig_pods..." 2>&1|tee -a $LOGFILE
             /app/www/html/"$ENVNAME"/vendor/bin/drush en -y structure_sync farm_map_mapbox cig_pods 2>&1|tee -a $LOGFILE
-		    wait
+                    wait
 
             echo "Importing taxonomies..." 2>&1|tee -a $LOGFILE
             /app/www/html/"$ENVNAME"/vendor/bin/drush import:taxonomies -y --choice=full 2>&1|tee -a $LOGFILE
-		    wait
+                    wait
 
         else
             # Recreate alive file link if needed
@@ -95,9 +124,9 @@ if [[ "$SUPPORTED_ENVS" == *"$2"* ]]; then
             echo "Run config import"
             /app/www/html/"$ENVNAME"/vendor/bin/drush cim 2>&1|tee -a $LOGFILE
         fi
-        
+
         echo "Changing application ownership to appadmin..." 2>&1|tee -a $LOGFILE
-        chown -R appadmin:appadmin /app/www/html 2>&1|tee -a $LOGFILE
+        chown -R appadmin:appadmin /app/www/html/"$ENVNAME" 2>&1|tee -a $LOGFILE
 
         echo "Running drush to clear and rebuild cache..." 2>&1|tee -a $LOGFILE
         /app/www/html/"$ENVNAME"/vendor/bin/drush cr 2>&1|tee -a $LOGFILE
